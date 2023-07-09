dadca341819c757a9695ccd15bbb1bc6220deb7a
Patrick <patrick@pop-os.localdomain>
2023-07-09 14:18:44 -0700

    possible solutions for 2.4 and 2.5 in the book.
    Added update function that uses exceptions, and find function that only performs one comparison per level.

--- lib/binary_tree.ex
+++ lib/binary_tree.ex
@@ -41,18 +41,88 @@ defmodule DataStructures.BinaryTree do
     Map.put(current, :right, add_node(right, new_val))
   end
 
+  @doc """
+  Exercise 2.3 & 2.4 in the book.
+
+  Here we keep an accumulator that tracks our path to where the new value should be inserted
+  and raise an exception once we have reached a nil tree value.  The trace is returned back 
+  up to the calling function via a RuntimeError (this is generally a bad idea, but here we 
+  are specifically trying to save memory over all else) and is then used to update the tree 
+  via Kernel.put_in/3.  This version hinges on put_in being more memory efficient than copying
+  a path down to the leaf where we insert the node as with add_node.
+
+  To redcue the number of comparisons we take the right branch as soon as we know that we aren't
+  taking the left branch instead of making a check to see if the new_value is less than the current 
+  node value.
+
+  """
+  @spec add_node_with_exception(t, term) :: t
+  def add_node_with_exception(tree, new_val) do
+    try do
+      add_node_with_exception(tree, new_val, [])
+    rescue
+      e in RuntimeError ->
+        case e.message do
+          :duplicate ->
+            tree
+
+          trace ->
+            put_in(tree, trace, new(new_val))
+        end
+    end
+  end
+
+  defp add_node_with_exception(nil, _new_val, trace) do
+    raise RuntimeError, message: Enum.reverse(trace)
+  end
+
+  defp add_node_with_exception(%__MODULE__{value: val}, val, _trace) do
+    raise RuntimeError, message: :duplicate
+  end
+
+  defp add_node_with_exception(%__MODULE__{value: val, left: left}, new_val, trace)
+       when new_val < val do
+    add_node_with_exception(left, new_val, [Access.key(:left) | trace])
+  end
+
+  defp add_node_with_exception(%__MODULE__{right: right}, new_val, trace) do
+    add_node_with_exception(right, new_val, [Access.key(:right) | trace])
+  end
+
   # base cases
   @spec value_in_tree?(t, term) :: boolean
   def value_in_tree?(%__MODULE__{value: val}, tar) when val == tar, do: true
   def value_in_tree?(nil, _tar), do: false
-  
-  #recursive cases
+
+  # recursive cases
   def value_in_tree?(%__MODULE__{value: val, left: left}, tar) when tar < val do
     value_in_tree?(left, tar)
   end
 
   def value_in_tree?(%__MODULE__{value: val, right: right}, tar) when tar > val do
     value_in_tree?(right, tar)
+  end
+
+  @doc """
+  Exercise 2.2 in the book.  We are aksed to make a version of search where we perform
+  at most d + 1 comaprisons where d is the depth of the binary tree.
+
+  By recording candidates for the match as we traverse the tree and only making a comparsion
+  at the end we ensure that the algorithm always makes d + 1 comparisons, but it will now
+  perform worse in the average case.
+  """
+  @spec lazy_value_in_tree?(t, term, term) :: boolean
+  def lazy_value_in_tree?(tree, tar, candidate \\ nil)
+  def lazy_value_in_tree?(nil, tar, candidate), do: tar == candidate
+
+  def lazy_value_in_tree?(%__MODULE__{value: val, left: left, right: right}, tar, candidate) do
+    # if the value is greater than the target then it is not a candidate.
+    if tar < val do
+      lazy_value_in_tree?(left, tar, candidate)
+    else
+      # in this case the val is <= the tar so it is a canddiate.
+      lazy_value_in_tree?(right, tar, val)
+    end
   end
 
   @doc """
--- test/binary_tree_test.exs
+++ test/binary_tree_test.exs
@@ -32,6 +32,30 @@ defmodule BinaryTreeTest do
     end
   end
 
+  describe "add_with_exception/2" do
+    test "add will place an element that is less than the current node to the left" do
+      assert BT.add_node_with_exception(BT.new(1), 0) == %BT{value: 1, left: BT.new(0), right: nil}
+    end
+
+    test "add will place an element that is greater than the current node to the right" do
+      assert BT.add_node_with_exception(BT.new(1), 2) == %BT{value: 1, right: BT.new(2), left: nil}
+    end
+
+    test "add will not add a duplicate value to a tree" do
+      assert BT.add_node_with_exception(BT.new(3), 3) == BT.new(3)
+    end
+
+    test "add will recursively insert recursively to the right" do
+      tree = BT.new(2) |> BT.add_node_with_exception(3) |> BT.add_node_with_exception(4)
+      assert tree == %BT{value: 2, left: nil, right: %BT{value: 3, right: BT.new(4), left: nil}}
+    end
+
+    test "add will recursively insert recursively to the left" do
+      tree = BT.new(4) |> BT.add_node_with_exception(3) |> BT.add_node_with_exception(2)
+      assert tree == %BT{value: 4, right: nil, left: %BT{value: 3, left: BT.new(2), right: nil}}
+    end
+  end
+
   describe "value_in_tree?/2" do
     test "will return true for values that are in the tree" do
       assert Enum.map(1..10, fn _ -> Enum.shuffle(1..10) end)
@@ -47,6 +71,23 @@ defmodule BinaryTreeTest do
              |> Enum.all?()
     end
   end
+
+  describe "lazy_value_in_tree?/2" do
+    test "will return true for values that are in the tree" do
+      assert Enum.map(1..10, fn _ -> Enum.shuffle(1..10) end)
+             |> Enum.map(&BT.tree_from_list/1)
+             |> Enum.map(&BT.lazy_value_in_tree?(&1, Enum.random(1..10)))
+             |> Enum.all?()
+    end
+
+    test "will return false for values that are not in the tree" do
+      refute Enum.map(1..10, fn _ -> Enum.shuffle(1..10) end)
+             |> Enum.map(&BT.tree_from_list/1)
+             |> Enum.map(&BT.lazy_value_in_tree?(&1, Enum.random(11..21)))
+             |> Enum.all?()
+    end
+  end
+
 
   # don't think much testing is needed her as we have already tested add thoroughly and the implmenation of 
   # tree_from_list is just repeated add calls.
